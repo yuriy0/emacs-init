12df47c070eea2312b6b231d79e6d014e130c503
yuriy <toporoy@mcmaster.ca>
2017-05-21 02:28:24 -0400

    Some (more) helm customization

--- customization/my-modes/helm.el
+++ customization/my-modes/helm.el
@@ -16,15 +16,17 @@
  (kbd "C-x b") 'helm-buffers-list
  (kbd "C-x q") 'helm-resume)
 
-(setq helm-M-x-fuzzy-match t        ; optional fuzzy matching for helm-M-x
-      helm-ff-newfile-prompt-p nil  ; don't ask to create new file
-      helm-display-header-line nil  ; no helm header 
-      helm-split-window-in-side-p t ; prevent helm from temporarily hiding other
-                                    ; buffers
+(setq helm-M-x-fuzzy-match t                  ; optional fuzzy matching for helm-M-x
+      helm-ff-newfile-prompt-p nil            ; don't ask to create new file
+      helm-display-header-line nil            ; no helm header 
+      helm-split-window-in-side-p t           ; prevent helm from temporarily hiding other
+                                              ; buffers
       helm-split-window-default-side 'below
       helm-apropos-fuzzy-match t
-      helm-default-external-file-browser "explorer.exe"
-      helm-buffer-max-length nil)   ; don't truncate buffer names 
+      helm-default-external-file-browser 
+        "explorer.exe"
+      helm-buffer-max-length nil              ; don't truncate buffer names 
+      helm-move-to-line-cycle-in-source t)
 
 ;; helm-ag
 (require 'helm-ag)
