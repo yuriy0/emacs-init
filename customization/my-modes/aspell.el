7f6ced04e31de867a7a943ed40a6475ce2a353b3
yuriy <toporoy@mcmaster.ca>
2017-05-21 03:05:42 -0400

    Move more files around

--- customization/my-modes/aspell.el
+++ customization/my-modes/aspell.el
@@ -2,4 +2,5 @@
 (setq ispell-program-name "aspell")
 (require 'ispell)
 (global-set-key (kbd "<f8>") 'ispell-word)
-(setq ispell-local-dictionary "british")
+(setq ispell-local-dictionary "british" 
+      ispell-personal-dictionary "~/emacs.d/.aspell.en.pws")
