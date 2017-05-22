31081f0fb0b4c633437b1440b7dd9f1eb245d128
yuriy <toporoy@mcmaster.ca>
2017-05-22 02:12:12 -0400

    Use the copy of maplev mode on github

--- customization/my-modes/maple.el
+++ customization/my-modes/maple.el
@@ -1,5 +1,5 @@
 ;; Maple 
-(setq load-path (cons "~/.emacs.d/maple" load-path))
+(setq load-path (cons "~/.emacs.d/maplev/lisp" load-path))
 (autoload 'maplev-mode "maplev" "Maple editing mode" t)
 (add-to-list 'auto-mode-alist '("\\.mpl\\'" . maplev-mode))
 (setq maplev-indent-level 2)
