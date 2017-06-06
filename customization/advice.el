b8d452fce795f5775563f97d6c42ec4041324cfa
yuriy <toporoy@mcmaster.ca>
2017-06-06 05:50:08 -0400

    Fix this later

--- customization/advice.el
+++ customization/advice.el
@@ -9,32 +9,32 @@
 ;;; Filters ido-matches setting acronynm matches in front of the results
 ;;; From the smex docs: http://www.emacswiki.org/emacs/Smex
 ;;;###autoload
-(defadvice ido-set-matches-1 (after ido-smex-acronym-matches activate)
-  (if (and (fboundp 'smex-already-running) (smex-already-running)
-           (> (length ido-text) 1))
-      (let ((regex (concat "^" (mapconcat 'char-to-string ido-text "[^-]*-")))
-            (acronym-matches (list))
-            (remove-regexes '("-menu-")))
-        ;; Creating the list of the results to be set as first
-        (dolist (item items)
-          (if (string-match ido-text item) ;; exact match
-              (add-to-list 'acronym-matches item)
-            (if (string-match (concat regex "[^-]*$") item) ;; strict match
-                (add-to-list 'acronym-matches item)
-              (if (string-match regex item) ;; appending relaxed match
-                  (add-to-list 'acronym-matches item t)))))
+;; (defadvice ido-set-matches-1 (after ido-smex-acronym-matches activate)
+;;   (if (and (fboundp 'smex-already-running) (smex-already-running)
+;;            (> (length ido-text) 1))
+;;       (let ((regex (concat "^" (mapconcat 'char-to-string ido-text "[^-]*-")))
+;;             (acronym-matches (list))
+;;             (remove-regexes '("-menu-")))
+;;         ;; Creating the list of the results to be set as first
+;;         (dolist (item items)
+;;           (if (string-match ido-text item) ;; exact match
+;;               (add-to-list 'acronym-matches item)
+;;             (if (string-match (concat regex "[^-]*$") item) ;; strict match
+;;                 (add-to-list 'acronym-matches item)
+;;               (if (string-match regex item) ;; appending relaxed match
+;;                   (add-to-list 'acronym-matches item t)))))
 
-        ;; Filtering ad-return-value
-        (dolist (to_remove remove-regexes)
-          (setq ad-return-value
-                (delete-if (lambda (item)
-                             (string-match to_remove item))
-                           ad-return-value)))
+;;         ;; Filtering ad-return-value
+;;         (dolist (to_remove remove-regexes)
+;;           (setq ad-return-value
+;;                 (delete-if (lambda (item)
+;;                              (string-match to_remove item))
+;;                            ad-return-value)))
 
-        ;; Creating resulting list
-        (setq ad-return-value
-              (append acronym-matches
-                      ad-return-value))
+;;         ;; Creating resulting list
+;;         (setq ad-return-value
+;;               (append acronym-matches
+;;                       ad-return-value))
 
-        (delete-dups ad-return-value)
-        (reverse ad-return-value))))
+;;         (delete-dups ad-return-value)
+;;         (reverse ad-return-value))))
