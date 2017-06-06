ade807a2edd5c60decf3f6dd29d2b940acf372b2
yuriy <toporoy@mcmaster.ca>
2017-06-06 05:51:45 -0400

    wg options

--- customization/my-modes/workgroups.el
+++ customization/my-modes/workgroups.el
@@ -20,10 +20,14 @@
 
 ;; Mode Line changes
 ;; Display workgroups in Mode Line?
-(setq wg-mode-line-display-on t)          ; Default: (not (featurep 'powerline))
-(setq wg-flag-modified t)                 ; Display modified flags as well
-(setq wg-mode-line-decor-left-brace "["
+(setq wg-mode-line-display-on t           ; Default: (not (featurep 'powerline))
+      wg-flag-modified t                  ; Display modified flags as well
+      wg-mode-line-decor-left-brace "["
       wg-mode-line-decor-right-brace "]"  ; how to surround it
-      wg-mode-line-decor-divider ":")
+      wg-mode-line-decor-divider ":"
+      wg-buffer-auto-association 'strong
+      wg-mess-with-buffer-list t
+      wg-remember-frame-for-each-wg t
+      wg-restore-remote-buffers nil)
 
 (workgroups-mode 1)   ; put this one at the bottom of .emacs
