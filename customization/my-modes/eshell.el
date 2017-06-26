9cdcde1e04ea212061d65cdb6546a6c2d8c0945b
yuriy <toporoy@mcmaster.ca>
2017-06-25 21:22:40 -0400

    Not loaded by eshell itself?

--- customization/my-modes/eshell.el
+++ customization/my-modes/eshell.el
@@ -1,6 +1,12 @@
 ;; eshell
 (require 'helm-eshell)
-(require 'em-prompt)
+
+;; just for *Help*
+(mapc #'require '(em-alias em-banner em-basic em-cmpl em-dirs
+      em-glob em-hist em-ls em-pred em-prompt em-rebind em-script
+      em-smart em-term em-tramp em-unix em-xtra esh-arg esh-cmd
+      esh-ext esh-groups esh-io esh-mode esh-module esh-opt
+      esh-proc esh-util esh-var))
 
 (add-hook 'eshell-mode-hook 
   (lambda () 
