3dffd19d2c16328f3729d0782873964e58129a36
yuriy <toporoy@mcmaster.ca>
2017-06-25 21:23:47 -0400

    Move all initilization into hook

--- customization/my-modes/eshell.el
+++ customization/my-modes/eshell.el
@@ -10,16 +10,17 @@
 
 (add-hook 'eshell-mode-hook 
   (lambda () 
-    (define-key eshell-mode-map (kbd "M-]") 'helm-eshell-history)
     (setq eshell-cmpl-ignore-case t)
     (eshell-cmpl-initialize)
     (set-face-attribute 'eshell-prompt nil :foreground "chartreuse4")
     (push-mark)
-    (define-key eshell-mode-map [remap eshell-pcomplete] 'helm-esh-pcomplete)
+    (define-keys eshell-mode-map 
+      (kbd "M-]") 'helm-eshell-history
+      [remap eshell-pcomplete] 'helm-esh-pcomplete
+      )
+    (setq eshell-prompt-function #'(lambda ()
+      (let ((sp (propertize " " 'face '(:background "#fff"))))
+        (concat
+         sp (abbreviate-file-name (eshell/pwd)) "\n"
+         sp (if (= (user-uid) 0) "#" "$") " ")) ))
     ))
-
-(setq eshell-prompt-function #'(lambda ()
-  (let ((sp (propertize " " 'face '(:background "#fff"))))
-    (concat
-     sp (abbreviate-file-name (eshell/pwd)) "\n"
-     sp (if (= (user-uid) 0) "#" "$") " ")) ))
