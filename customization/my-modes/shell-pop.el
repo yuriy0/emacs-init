bb57d73397ee5a6125be005bcf5a0442a2516abe
yuriy <toporoy@mcmaster.ca>
2017-07-03 02:39:47 -0400

    Override this shell-pop function to customize location behaviour

--- customization/my-modes/shell-pop.el
+++ customization/my-modes/shell-pop.el
@@ -4,6 +4,31 @@
 (global-set-keys (kbd "C-'") 'shell-pop)
 (setq shell-pop-restore-window-configuration nil)
 
+(defun extremal-windows (dirs)
+  "Computes the windows in the current frame farthest in the
+directions specified. `DIRS' must be a non-empty list of
+directions (a direction being that which which can be passed to
+`window-in-direction') containing no opposite directions."
+  (when (or (eq dirs nil)
+            (cl-subsetp '(above below) dirs)
+            (cl-subsetp '(left right) dirs)
+            (not (cl-subsetp dirs '(above left right below))))
+    (user-error "Invalid set of directions: %s" dirs))
+  (--filter
+   (every
+    (lambda(d) (not (window-in-direction d it))) dirs)
+   (window-list)))
+
+(defun my--shell-pop-split-window ()
+  (unless (shell-pop--full-p)
+    (let ((win (car (extremal-windows '(right below)))))
+      (setq shell-pop-window-position
+            (if (eq (count-windows) 1) "right" "bottom"))
+      (split-window
+       win
+       (shell-pop--calculate-window-size)
+       (shell-pop--translate-position shell-pop-window-position)))))
+(fset 'shell-pop-split-window 'my--shell-pop-split-window)
 
 (defun shell-pop--shell-buffer-name (index)
   (if (string-match-p "*\\'" shell-pop-internal-mode-buffer)
