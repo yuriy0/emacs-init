6bcbdbb200c40c807d7bfa77ff06f43deb1d162a
yuriy <toporoy@mcmaster.ca>
2017-07-03 02:39:24 -0400

    Disable restoring window config (it's just confusing...)

--- customization/my-modes/shell-pop.el
+++ customization/my-modes/shell-pop.el
@@ -1,7 +1,9 @@
 ;; shell pop + eshell
-(customize-set-variable 
+(customize-set-variable
   'shell-pop-shell-type '("eshell" "*eshell*" (lambda () (eshell))) )
 (global-set-keys (kbd "C-'") 'shell-pop)
+(setq shell-pop-restore-window-configuration nil)
+
 
 (defun shell-pop--shell-buffer-name (index)
   (if (string-match-p "*\\'" shell-pop-internal-mode-buffer)
