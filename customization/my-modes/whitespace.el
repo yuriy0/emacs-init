55e017dcbc3a1f1c44b2ae99911f221baefe7753
yuriy <toporoy@mcmaster.ca>
2017-07-03 00:43:14 -0400

    Use whitespace-mode for max column length

--- customization/my-modes/whitespace.el
+++ customization/my-modes/whitespace.el
@@ -1,5 +1,16 @@
-;; whitespace mode 
+;; whitespace mode
 (require 'whitespace)
-(setq whitespace-style '(tabs tab-mark trailing))
-(setq-default whitespace-line-column 80)
-;; (global-whitespace-mode 1)
+(setq-default
+ whitespace-line-column nil
+ whitespace-style       '(face lines-tail tabs tab-mark trailing))
+(setq global-whitespace-mode '(prog-mode (not agda2-mode)))
+
+;; https://www.emacswiki.org/emacs/WhiteSpace
+(setq whitespace-display-mappings
+  '((space-mark   ?\    [?\xB7]     [?.])     ; space
+    (space-mark   ?\xA0 [?\xA4]     [?_])     ; hard space
+    (newline-mark ?\n   [?\xB6 ?\n] [?$ ?\n]) ; end-of-line
+    ))
+
+(set-face-attribute 'whitespace-trailing nil
+   :background "bisque")
