f8c77c0750e3e531bce1253d11e6c6a59f3549be
yuriy <toporoy@mcmaster.ca>
2017-07-13 13:41:30 -0400

    formatting

--- customization/my-modes/linum.el
+++ customization/my-modes/linum.el
@@ -1,5 +1,6 @@
 ;; line numbers 
 ;;;###autoload
-(defun enable-linum-mode-in-mode (the-mode) (add-hook the-mode (lambda () (linum-mode 1))))
-(-each '(prog-mode-hook haskell-mode-hook maplev-mode-hook) 
+(defun enable-linum-mode-in-mode (the-mode)
+  (add-hook the-mode (lambda () (linum-mode 1))))
+(-each '(prog-mode-hook haskell-mode-hook maplev-mode-hook)
        'enable-linum-mode-in-mode)
