f6b22006bbda2f54efa11150816d7e1d55463a6e
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2018-02-05 14:24:22 -0500

    Better ace jump keys

--- customization/my-modes/ace-jump.el
+++ customization/my-modes/ace-jump.el
@@ -1,5 +1,6 @@
 ;; jumping
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-word-mode)
+(define-key global-map (kbd "C-c C-SPC") 'ace-jump-char-mode)
 
 ;; zapping
 (global-set-key [remap zap-to-char] 'ace-jump-zap-up-to-char)
