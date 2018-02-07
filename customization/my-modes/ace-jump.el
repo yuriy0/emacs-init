c8712815a604d0134e025d99c8afb71d7bfc1ee2
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2018-02-07 18:19:35 +0000

    Use char mode more often so give it the easier keybinding

--- customization/my-modes/ace-jump.el
+++ customization/my-modes/ace-jump.el
@@ -1,6 +1,6 @@
 ;; jumping
-(define-key global-map (kbd "C-c SPC") 'ace-jump-word-mode)
-(define-key global-map (kbd "C-c C-SPC") 'ace-jump-char-mode)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-char-mode)
+(define-key global-map (kbd "C-c C-SPC") 'ace-jump-word-mode)
 
 ;; zapping
 (global-set-key [remap zap-to-char] 'ace-jump-zap-up-to-char)
