eaae197e4259f0966123fb37dde4e5a619b5627c
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2018-02-20 15:59:52 -0500

    Actually enable mode

--- customization/my-modes/ido.el
+++ customization/my-modes/ido.el
@@ -1,5 +1,7 @@
-;; ido mode 
+;; ido mode
 (setq ido-save-directory-list-file "~/.emacs.d/.ido.last")
 (require 'ido)
-(ido-mode t) 
+(ido-mode t)
+
 (require 'ido-yes-or-no)
+(setq ido-yes-or-no-mode t)
