3b92b94280b79349a71ed197a27ded830da93e3a
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2018-07-17 19:38:55 +0100

    Set tab width to 4 and tab with spaces

--- customization/my-modes/c.el
+++ customization/my-modes/c.el
@@ -1,3 +1,13 @@
 ;; C
-(require 'cc-mode) 
+(require 'cc-mode)
 
+(add-hook 'c-mode-common-hook
+  #'(lambda()
+      (setq
+       c-default-style "bsd"
+       c-basic-offset 4
+       tab-width 4
+       indent-tabs-mode nil
+       )
+    )
+)
