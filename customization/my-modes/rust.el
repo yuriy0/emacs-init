9e2e973679a398bebd02fdc398d2ae42ce84f27e
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2022-10-24 16:23:10 +0100

    fix broken config

--- customization/my-modes/rust.el
+++ customization/my-modes/rust.el
@@ -1,50 +1,100 @@
 ;; see https://github.com/rksm/emacs-rust-config/blob/master/init.el
-
-
 (use-package rustic
   :ensure
-
-  :init
-  (setq rustic-lsp-setup-p nil) ;; disables auto setup which seems to break any further configuration?
+  :bind
+  (:map rustic-mode-map
+        ("M-j" . lsp-ui-imenu)
+        ("C-c C-c a" . lsp-execute-code-action)
+        )
 
   :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; find rust analyzer
   (progn
-    ;; find rust analyzer
     (setq lsp-rust-server 'rust-analyzer)
     (let ((rust-analyzer-path
            (shell-command-to-string  "rustup which --toolchain stable rust-analyzer")))
       (setq rustic-analyzer-command (list rust-analyzer-path))
       )
    )
+
+  ;; indent settings
   (add-hook 'rustic-mode-hook
     (lambda ()
-
-      ;; indent settings
       (setq indent-tabs-mode nil)
       (setq tab-width 2)
       (setq default-tab-width 2)
       (setq rust-indent-offset 2)
-
-      ;; don't need in rust mode, the rust analyzer gives very good results
-      ;; (setq-local company-backends
-      ;;    (remove 'company-files (
-      ;;     remove 'company-etags (
-      ;;     remove 'company-dabbrev company-backends)))
-      ;;  )
     ))
+)
 
-  :bind (:map
-         rustic-mode-map
-         ("C-c C-c a" . lsp-execute-code-action))
+(use-package lsp-mode
+  :ensure
+  :commands lsp
+  :custom
+
+  ;;"lens" = count references to symbols
+  (lsp-lens-enable t)
+
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+
+  ;; This controls the overlays that display type and other hints inline. Enable
+  ;; / disable as you prefer. Well require a `lsp-workspace-restart' to have an
+  ;; effect on open projects.
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+
+  ;; after changes clear diagnostics since they will usually refer to invalid line/column numbers
+  (lsp-diagnostic-clean-after-change t)
+
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+
+  :bind
+  ("C-c C-c s" . #'toggle-lsp-ui-sideline-show-hover)
+
+  ;; standard xref jump using lsp-ui instead
+  (([remap xref-find-definitions] . #'lsp-ui-peek-find-definitions)
+   ([remap xref-find-references] . #'lsp-ui-peek-find-references)
+   )
+
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-doc-enable nil)
+  (lsp-ui-sideline-show-hover nil)
+  (lsp-ui-sideline-show-diagnostics t)
+)
+
+(defun toggle-lsp-ui-sideline-show-hover ()
+  (interactive)
+  (setq lsp-ui-sideline-show-hover (not lsp-ui-sideline-show-hover))
 )
 
 (use-package flycheck :ensure)
 
-;; ;;;###autoload
-;; (defun company-abort-then-complete ()
-;;   (interactive)
-;;   (call-interactively 'company-complete)
-;; )
+(use-package yasnippet
+  :ensure
+  :config
+  (yas-reload-all)
+  (add-hook 'rustic-mode-hook 'yas-minor-mode)
+  )
 
 (use-package company
   :ensure
@@ -52,10 +102,7 @@
   :custom
 
   ;; idle completion
-  (company-idle-delay 0.0)
-
-  ;; (company-idle-delay 0)
-  ;; (company-tooltip-idle-delay 9999)
+  (company-idle-delay (lambda() (if (company-in-string-or-comment) nil 0.0)))
 
   ;; completion starts with any # of characters
   (company-minimum-prefix-length 1)
@@ -77,70 +124,4 @@
         ;; ("TAB" . company-complete-selection)
         ("<tab>" . company-complete-selection)
         )
-
-  ;; :config
-  ;; (add-hook 'company-completion-started-hook
-  ;;           (lambda (is-manual)
-  ;;             (if is-manual
-  ;;                 (setq company-tooltip-idle-delay 0)
-  ;;               )
-  ;;             )
-  ;;           )
-  ;; (add-hook 'company-after-completion-hook
-  ;;           (lambda (s)
-  ;;             (setq company-tooltip-idle-delay 99999)
-  ;;             )
-  ;;           )
-)
-
-;; (with-eval-after-load 'company
-;;   (define-key company-active-map (kbd "M-/") #'company-complete))
-
-(use-package lsp-mode
-  :ensure
-  :commands lsp
-
-  :custom
-  (lsp-idle-delay 0.65)
-
-  ;;"lens" = count references to symbols
-  (lsp-lens-enable t)
-
-  ;;interferes with diagnostics from flycheck
-  (lsp-eldoc-enable-hover nil)
-
-  ;; This controls the overlays that display type and other hints inline. Enable
-  ;; / disable as you prefer. Well require a `lsp-workspace-restart' to have an
-  ;; effect on open projects.
-  (lsp-rust-analyzer-server-display-inlay-hints t)
-  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
-  (lsp-rust-analyzer-display-chaining-hints nil)
-  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names t)
-  (lsp-rust-analyzer-display-closure-return-type-hints t)
-  (lsp-rust-analyzer-display-parameter-hints nil)
-  (lsp-rust-analyzer-display-reborrow-hints t)
-
-  ;; after changes clear diagnostics since they will usually refer to invalid line/column numbers
-  (lsp-diagnostic-clean-after-change t)
-
-  :config
-  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
-  (add-hook 'lsp-mode-hook 'flycheck-mode)
-)
-
-(use-package lsp-ui
-  :ensure
-  :commands lsp-ui-mode
-
-  :bind
-  ;; standard xref jump using lsp-ui instead
-  (([remap xref-find-definitions] . #'lsp-ui-peek-find-definitions)
-   ([remap xref-find-references] . #'lsp-ui-peek-find-references)
-   )
-
-  :custom
-  (lsp-ui-peek-always-show t)
-  (lsp-ui-doc-enable nil)
-  (lsp-ui-sideline-show-hover nil)
-  (lsp-ui-sideline-show-diagnostics t)
 )
