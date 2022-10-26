83c3620fd07775fe9d22c26f030eddf5eb69d92d
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2022-10-26 17:33:14 +0100

    Defer unicode-fonts-setup until an appropriate time

--- customization/my-modes/font.el
+++ customization/my-modes/font.el
@@ -2,17 +2,47 @@
   :ensure
   :if (display-graphic-p))
 
-
 ;; fixes an issue with emacs hanging when viewing some unicode files
 ;; see https://github.com/purcell/emacs.d/issues/273
 (setq inhibit-compacting-font-caches t)
 
+(use-package ucs-utils :ensure)
+
+(use-package descr-text :ensure)
+
+
+;;;###autoload
+(defun any-non-displayable ()
+  (require 'descr-text) ;; for `describe-char-display'
+  (setq found nil)
+  (save-excursion
+    (goto-char (point-min))
+
+    (while (and (not found) (not (eobp)))
+      (if (or (eolp)
+              (looking-at "\t")
+              (describe-char-display (point) (char-after)))
+          (forward-char)
+        (setq found t)
+        ))
+    )
+  found)
+
+;;;###autoload
+(defun maybe-unicode-fonts-setup (&rest _unused)
+  (when (any-non-displayable)
+    (unicode-fonts-setup)
+    (remove-hook 'window-buffer-change-functions 'maybe-unicode-fonts-setup
+    )))
+
 (use-package unicode-fonts
   :ensure
+  :requires (descr-text)
 
   :config
-  (unicode-fonts-setup)
-)
 
-(use-package ucs-utils :ensure)
+  ;; the first time we open a buffer which has non-printing characters, we'll run `unicode-fonts-setup'
+  ;; if we run it too early it won't pick up some future faces which might be added by deferred setup
+  (add-hook 'window-buffer-change-functions #'maybe-unicode-fonts-setup)
+)
 
