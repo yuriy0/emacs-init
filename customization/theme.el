2d95b4d35172fa4d24cd4c5eff147961f3e38453
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2022-10-27 15:59:47 +0100

    Use default theme for smart mode line

--- customization/theme.el
+++ customization/theme.el
@@ -1,15 +1,5 @@
 (use-package smart-mode-line
   :ensure
   :config
-
-  (setq sml/theme 'light)
   (sml/setup)
-)
-
-(use-package smart-mode-line-powerline-theme
-  :ensure
-  :after (smart-mode-line)
-  :config
-
-  (sml/apply-theme 'light-powerline)
 )
