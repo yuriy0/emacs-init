e10342e3a334dd631dc06509abdbdff2d7ef0862
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2022-11-21 17:11:31 -0500

    Flycheck error list hydra

--- customization/my-modes/flycheck.el
+++ customization/my-modes/flycheck.el
@@ -1,6 +1,27 @@
 (use-package flycheck
   :ensure
   :commands (flycheck-mode)
+  :autoload (hydra-flycheck-with-errs-list)
 
   :delight flycheck-mode
+
+  :bind
+  (:map flycheck-mode-map
+        ("C-c !" . #'hydra-flycheck-with-errs-list))
+)
+
+;;;###autoload
+(defun hydra-flycheck-with-errs-list ()
+  "Opens flycheck error list buffer and starts flycheck navigation hydra"
+  (interactive)
+  (flycheck-list-errors)
+  (hydra-flycheck/body))
+
+(defhydra hydra-flycheck ()
+  "Move around flycheck errors"
+  ("n" flycheck-next-error "next")
+  ("p" flycheck-previous-error "prev")
+  ("f" flycheck-first-error "first")
+  ("q" nil "break"))
+
 )
---
+++ customization/my-modes/hydra.el
@@ -0,0 +1,3 @@
+(use-package hydra
+  :defer 10
+  :commands (hydra-flycheck/body))
