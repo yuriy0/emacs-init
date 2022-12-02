3f17e792c5ae5321fe837a875ecd1346d2113c6a
Yuriy Toporovskyy <yuriy.toporovskyy@apocalypse333.com>
2022-12-02 17:46:11 -0500

    Some keybinds not working in tty on windows...

--- customization/my-modes/expand-region.el
+++ customization/my-modes/expand-region.el
@@ -1,4 +1,10 @@
 (use-package expand-region
   :ensure t
-  :bind ("C-=" . er/expand-region)
-)
+  :commands (er/expand-region)
+
+  :config
+  (bind-keys
+   `(
+     ,(if (display-graphic-p) "C-=" "C-]") ;; TODO: how to use all keybinds correctly in TTY?
+     . er/expand-region))
+  )
