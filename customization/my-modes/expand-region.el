5dd16418ebcecef70dc7ac6b2e2e82979029a744
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2022-12-03 15:50:22 -0500

    Yet another attempt at handling keybinds in TTY mode.
    This time, if each new frame is a tty display, then bind the alternate key; and always bind the desired key. Note that display-graphics-p is nil when starting the daemon!

--- customization/my-modes/expand-region.el
+++ customization/my-modes/expand-region.el
@@ -4,8 +4,20 @@
 
   :init
   (bind-keys
-   ((if (display-graphic-p) "C-=" "C-]") ;; TODO: how to use all keybinds correctly in TTY?
-    . er/expand-region))
+   ("C-=" . er/expand-region)
+   )
+
+  (add-hook
+   'after-make-frame-functions
+   (lambda ()
+     ;; in TTY frames on Windows Terminal and other terminal emulators,
+     ;; some keybinds like `C-=' don't work, so when such a frame is created,
+     ;; bind a different key...
+     ;; TODO: we should figure out how to use all keybinds correctly in TTY?
+     (when (not (display-graphic-p))
+       (bind-keys ("C-]" . er/expand-region)))
+     )
+   )
 
   :config
   ;; empty
