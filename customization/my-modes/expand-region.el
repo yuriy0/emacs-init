af7f866379a59187fed95a5ef28dc4de5499d195
Yuriy Toporovskyy <yuriy.toporovskyy@apocalypse333.com>
2022-12-03 15:42:17 -0500

    Bug fix: bind keys in :init, not :config, so that autoload will work correctly

--- customization/my-modes/expand-region.el
+++ customization/my-modes/expand-region.el
@@ -2,8 +2,11 @@
   :ensure t
   :commands (er/expand-region)
 
-  :config
+  :init
   (bind-keys
    ((if (display-graphic-p) "C-=" "C-]") ;; TODO: how to use all keybinds correctly in TTY?
     . er/expand-region))
+
+  :config
+  ;; empty
   )
