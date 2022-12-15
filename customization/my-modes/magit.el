855979d427fe718a665ae3350b015eb45486c75a
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2022-12-15 10:27:15 -0500

    Some magit settings

--- customization/my-modes/magit.el
+++ customization/my-modes/magit.el
@@ -3,4 +3,8 @@
 
   ;; maybe we can add more things here but most git workflows start with "status"
   :commands (magit-status)
+
+  :custom
+  (magit-auto-revert-mode nil)
+  (magit-diff-refine-hunk 'all)
 )
