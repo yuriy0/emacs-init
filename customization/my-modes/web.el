a6b805800e82d95a4fbf95d391f018561500caa3
Yuriy Toporovskyy <toporovskyy.y@gmail.com>
2022-12-22 21:29:54 +0000

    Use built in modes in latest emacs which has treesitter

--- customization/my-modes/web.el
+++ customization/my-modes/web.el
@@ -1,28 +1,9 @@
-;; Web mode
-(use-package web-mode
+;; lsp for javascript & typescript
+(use-package lsp-javascript
   :ensure
-  :commands (web-mode)
-
-  :init
-  (mapc (lambda(f)
-          (add-to-list 'auto-mode-alist `(,f . web-mode)))
-        '("\\.jsx?$"
-          "\\.js?$"
-          "\\.html?$"
-          "\\.css?$"
-          "\\.ts?$"
-          "\\.tsx?$"
-          ))
+  :defer t ;; this is actually loaded by the appropriate major mode
 
   :config
-
-  (define-key web-mode-map (kbd "RET") 'electric-indent-just-newline)
-  (add-hook 'web-mode-hook #'lsp-deferred)
-
-  (defun my/web-mode-hook()
-    (setq-local lsp-ui-peek-fontify 'always) ;; lsp-ui-peek breaks for web-mode files because the fontification only happens for file-visiting buffers
-    )
-  (add-hook 'web-mode-hook #'my/web-mode-hook)
 
   ;; todo: is this every useful in JS? everything has type `any' ...
   (progn
@@ -35,14 +16,6 @@
     (setq lsp-javascript-display-return-type-hints t)
     (setq lsp-javascript-implicit-project-config-check-js t)
     )
-
-  (setq web-mode-enable-auto-indentation nil)
-  (setq js-indent-level 2) ;; used by json files
-
-  ;; needed to get LSP to consider the indent size the same as which we set above
-  ;; https://github.com/emacs-lsp/lsp-mode/issues/2915#issuecomment-855156802
-  (with-eval-after-load 'lsp-mode
-    (setf (alist-get 'web-mode lsp--formatting-indent-alist) 'web-mode-code-indent-offset))
 
   (defvar lsp-javascript-extra-lines-range-for-inlay-hints 50)
   (defvar lsp-javascript-inlay-hints-on-scroll-rate-limit 0.5)
@@ -74,6 +47,49 @@
      'lsp-javascript-update-inlay-hints-scroll-function
      (my/lsp-javascript-update-inlay-hints-with-expansion
       start (window-end window t))))
+)
+
+(use-package js
+  :config
+  (setq js-indent-level 2) ;; used by json files
+)
+
+(defun my/use-web-mode() (version< emacs-version "29.0"))
+
+;; Web mode
+(use-package web-mode
+  :if (my/use-web-mode)
+  :ensure t
+  :commands (web-mode)
+
+  :init
+  (when (version< emacs-version "29.0")
+    (mapc (lambda(f)
+            (add-to-list 'auto-mode-alist `(,f . web-mode)))
+          '("\\.jsx?$"
+            "\\.js?$"
+            "\\.html?$"
+            "\\.css?$"
+            "\\.ts?$"
+            "\\.tsx?$"
+          )))
+
+  :config
+
+  (define-key web-mode-map (kbd "RET") 'electric-indent-just-newline)
+  (add-hook 'web-mode-hook #'lsp-deferred)
+
+  (defun my/web-mode-hook()
+    (setq-local lsp-ui-peek-fontify 'always) ;; lsp-ui-peek breaks for web-mode files because the fontification only happens for file-visiting buffers
+    )
+  (add-hook 'web-mode-hook #'my/web-mode-hook)
+
+  (setq web-mode-enable-auto-indentation nil)
+
+  ;; needed to get LSP to consider the indent size the same as which we set above
+  ;; https://github.com/emacs-lsp/lsp-mode/issues/2915#issuecomment-855156802
+  (with-eval-after-load 'lsp-mode
+    (setf (alist-get 'web-mode lsp--formatting-indent-alist) 'web-mode-code-indent-offset))
 
   :custom
   (web-mode-markup-indent-offset 2)
@@ -85,3 +101,9 @@
   :ensure
   :after (web-mode)
 )
+
+(use-package typescript-ts-mode
+  :if (not (my/use-web-mode))
+  :commands (typescript-ts-mode)
+  :config
+  (add-hook 'typescript-ts-mode-hook #'lsp-deferred))
